ac1bada72fa4839f608aab1b148637347037cd73
Stephane Alnet <stephane@shimaore.net>
2016-10-18 14:13:26 +0200

    add counter
    ... to explore what happens after we're done with PouchDB

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var PouchDB = require('pouchdb-core')
 
 var plugin = null
+var options = {}
 
 switch(process.argv[2]) {
   case 'http':
-    console.log('Using the regular HTTP adapter.')
+    console.log('Using the regular HTTP adapter. Options = ',options)
     plugin = require('pouchdb-adapter-http')
     break;
   case 'dummy':
@@ -21,13 +22,14 @@ PouchDB.plugin(plugin)
 
 var heapUsed = null;
 
+var counter = 0;
 var measure = function() {
   var memory = process.memoryUsage();
   var last_heapUsed = heapUsed;
   heapUsed = memory.heapUsed;
 
   if (last_heapUsed !== null) {
-    console.log('current is',heapUsed,', difference is', heapUsed - last_heapUsed);
+    console.log('current is',heapUsed,', difference is', heapUsed - last_heapUsed, ', counter is',counter);
   }
 }
 
@@ -35,15 +37,22 @@ var run = setInterval(function () {
 
   global.gc();
 
-  var db = new PouchDB('http://127.0.0.1:5984/foo');
-  db
-    .info()
-    .then(function(){
-      db
-      .close()
-      .then(measure)
-    })
 
+  if(counter < 120) {
+    counter++;
+
+    var db = new PouchDB('http://127.0.0.1:5984/foo',options);
+    db
+      .info()
+      .then(function(){
+        db
+        .close()
+        .then(measure)
+      })
+
+  } else {
+    measure()
+  }
 }, 250);
 
 setTimeout(function(){
